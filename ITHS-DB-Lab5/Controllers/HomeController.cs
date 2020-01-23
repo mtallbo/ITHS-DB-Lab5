0f70c228829db4a83e598fac02b24a36f25fcea5
mikael.tallbo <mikael.tallbo@gmail.com>
2020-01-23 18:55:19 +0100

    Added front page blog posts and minor fixes

--- ITHS-DB-Lab5/Controllers/HomeController.cs
+++ ITHS-DB-Lab5/Controllers/HomeController.cs
@@ -5,14 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ITHS_DB_Lab5.Models;
+using ITHS_DB_Lab5.Service;
 
 namespace ITHS_DB_Lab5.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly PostService postService;
+
+        public HomeController(PostService postService)
+        {
+            this.postService = postService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(postService.Get().OrderBy(p => p.DateCreated).Take(5));
         }
 
         public IActionResult Privacy()
